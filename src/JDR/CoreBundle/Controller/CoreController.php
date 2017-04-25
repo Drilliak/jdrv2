a8d81c0ab85013388a73f1b18849bf6c4d09149e
Drilliak <vinbat@hotmail.fr>
2017-04-25 13:14:33 +0200

    test

--- src/JDR/CoreBundle/Controller/CoreController.php
+++ src/JDR/CoreBundle/Controller/CoreController.php
@@ -11,15 +11,9 @@ class CoreController extends Controller
 {
     public function indexAction(){
 
-        $repository = $this->getDoctrine()->getManager()->getRepository('JDRUserBundle:User');
 
-        $user = $repository->find(1);
+        
 
-
-        var_dump($user->getCharacters()[0]->getName());
-
-
-
-        return $this->render("JDRCoreBundle:Core:index.html.twig");
+        return $this->render("JDRCoreBundle:Core:index.html.twig", array("coucou" => "salut ducon"));
     }
 }
